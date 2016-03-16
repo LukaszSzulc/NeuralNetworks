03727f75d27810d0eca32aadc02e6db68e0ee04f
Lukasz Szulc <lukasz.szulc@studentpartner.com>
2016-03-16 23:36:19 +0100

    Fix text format

--- NeuralNetworks.Tests/Bcm.cs
+++ NeuralNetworks.Tests/Bcm.cs
@@ -6,11 +6,11 @@
 
 	public class Bcm
 	{
-	    const int Size = 5;
+		const int Size = 5;
 		[Fact]
 		public void MatrixTrainedWithVectorShouldContainsTwoNotZeroRows()
 		{
-		    var vector = new[] { 1,1,0,0,0};
+			var vector = new[] { 1,1,0,0,0};
 			var bcmModel = new BcmModel(Size);
 
 			bcmModel.Train(vector);
@@ -18,48 +18,48 @@
 			Assert.Equal(new [] {1,1,0,0,0},bcmModel.CorrelationMatrix[0]);
 		}
 
-	    [Fact]
-	    public void TraningMatrixWithSecondVectorShouldNotChangeFirstRow()
-	    {
-            var firstVector = new[] { 1, 1, 0, 0, 0 };
-            var secondVector = new[] { 0, 1, 0, 0, 1 };
-            var bcmModel = new BcmModel(Size);
+		[Fact]
+		public void TraningMatrixWithSecondVectorShouldNotChangeFirstRow()
+		{
+			var firstVector = new[] { 1, 1, 0, 0, 0 };
+			var secondVector = new[] { 0, 1, 0, 0, 1 };
+			var bcmModel = new BcmModel(Size);
 
-            bcmModel.Train(firstVector);
+			bcmModel.Train(firstVector);
 			bcmModel.Train(secondVector);
 
-            Assert.Equal(new[] { 1, 1, 0, 0, 0 }, bcmModel.CorrelationMatrix[0]);
-            Assert.Equal(new[] { 1, 1, 0, 0, 1 }, bcmModel.CorrelationMatrix[1]);
+			Assert.Equal(new[] { 1, 1, 0, 0, 0 }, bcmModel.CorrelationMatrix[0]);
+			Assert.Equal(new[] { 1, 1, 0, 0, 1 }, bcmModel.CorrelationMatrix[1]);
 
-        }
+		}
 
-	    [Fact]
-	    public void TrainedMatrixShouldKnowProvidedVector()
-	    {
-            var firstVector = new[] { 1, 1, 0, 0, 0 };
-            var secondVector = new[] { 0, 1, 0, 0, 1 };
-            var bcmModel = new BcmModel(Size);
-            bcmModel.Train(firstVector);
-            bcmModel.Train(secondVector);
+		[Fact]
+		public void TrainedMatrixShouldKnowProvidedVector()
+		{
+			var firstVector = new[] { 1, 1, 0, 0, 0 };
+			var secondVector = new[] { 0, 1, 0, 0, 1 };
+			var bcmModel = new BcmModel(Size);
+			bcmModel.Train(firstVector);
+			bcmModel.Train(secondVector);
 
-	        var resultOfTest = bcmModel.Test(new[] { 0, 1, 0, 0, 1 });
+			var resultOfTest = bcmModel.Test(new[] { 0, 1, 0, 0, 1 });
 
-            Assert.True(resultOfTest);
-	    }
+			Assert.True(resultOfTest);
+		}
 
 
-        [Fact]
-        public void TrainedMatrixShouldNotKnowProvidedVector()
-        {
-            var firstVector = new[] { 1, 1, 0, 0, 0 };
-            var secondVector = new[] { 0, 1, 0, 0, 1 };
-            var bcmModel = new BcmModel(Size);
-            bcmModel.Train(firstVector);
-            bcmModel.Train(secondVector);
+		[Fact]
+		public void TrainedMatrixShouldNotKnowProvidedVector()
+		{
+			var firstVector = new[] { 1, 1, 0, 0, 0 };
+			var secondVector = new[] { 0, 1, 0, 0, 1 };
+			var bcmModel = new BcmModel(Size);
+			bcmModel.Train(firstVector);
+			bcmModel.Train(secondVector);
 
-            var resultOfTest = bcmModel.Test(new[] { 0, 0, 0, 0, 1 });
+			var resultOfTest = bcmModel.Test(new[] { 0, 0, 0, 0, 1 });
 
-            Assert.False(resultOfTest);
-        }
-    }
+			Assert.False(resultOfTest);
+		}
+	}
 }
